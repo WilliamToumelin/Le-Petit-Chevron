68ff607fa9f2ef4b3ae412a5e4156e5278c3f0c3
WilliamToumelin <william.toumelin@oclock.school>
2023-06-29 14:14:11 +0200

    Challenge fini

--- js/app.js
+++ js/app.js
@@ -1,156 +1,157 @@
 var app = {
   init: function () {
-    console.log('init');
+    console.log("init");
 
     // TODO
     app.drawBoard();
 
     // Event listeners - TODO
-    const launchScript = document.querySelector('#launchScript')
-    launchScript.addEventListener('click', app.handleLaunchScriptButton)
+    const launchScript = document.querySelector("#launchScript");
+    launchScript.addEventListener("click", app.handleLaunchScriptButton);
+    resetScript.addEventListener("click", app.handleResetScriptButton);
   },
 
   drawBoard: function () {
-    // création de la grille 
-
-    const board = document.querySelector('#board');
-    const rows = 4;
-    const columns = 6;
+    // création de la grille
+    const board = document.querySelector("#board");
+    const rows = 6;
+    const columns = 8;
 
     for (let i = 0; i < rows; i++) {
-      const cellRow = document.createElement('div');
-      cellRow.classList.add('cellRow');
-      cellRow.setAttribute('id', "row"+i);
+      const cellRow = document.createElement("div");
+      cellRow.classList.add("cellRow");
+      cellRow.setAttribute("id", "row" + i);
 
       for (let j = 0; j < columns; j++) {
-        const cellColumn = document.createElement('div');
-        cellColumn.classList.add('cell');
+        const cellColumn = document.createElement("div");
+        cellColumn.classList.add("cell");
         cellColumn.dataset.column = j;
         cellColumn.dataset.row = i;
-        cellRow.append(cellColumn)
+        cellRow.append(cellColumn);
       }
       board.append(cellRow);
     }
 
-    // Case de départ
-    const row1 = document.querySelector('#row0');
-    row1.firstChild.classList.add('cellStart');
+    // Case de départ aléatoire avec le curseur
+    const cells = document.querySelectorAll('.cell');
+    console.log(cells)
+    let cellStartRandom = Math.floor(Math.random() * cells.length);
+    const randomStart = cells[cellStartRandom];
+    randomStart.classList.add("cellStart")
 
     // La fléche est sur le départ
-    const curseur = document.querySelector('#row0');
-    curseur.firstChild.classList.add('cellCurrent');
-    curseur.firstChild.classList.add('cellCurrent-right');
+    randomStart.classList.add("cellCurrent");
+    randomStart.classList.add("cellCurrent-right");
 
     // la case d'arrivée
-    const row4 = document.querySelector('#row3');
-    row4.lastChild.classList.add('cellEnd');
-
-    app.moveForward();
-    app.turnRight();
-    // app.moveForward();
-    // app.turnLeft();
-    // app.moveForward();
-    // app.moveForward();
-    // app.turnRight();
-    // app.moveForward();
-    // app.turnLeft();
-    // app.moveForward();
-    // app.moveForward();
-    // app.turnRight();
-    // app.moveForward();
+    let cellEndRandom = Math.floor(Math.random() * cells.length);
+    const randomEnd = cells[cellEndRandom];
+    randomEnd.classList.add("cellEnd")
   },
 
   moveForward: function () {
-    const cellCurrent = document.querySelector('.cellCurrent');
+    const cellCurrent = document.querySelector(".cellCurrent");
 
     // dans quelle direction est le curseur
-    const top = cellCurrent.classList.contains('cellCurrent-top');
-    const bottom = cellCurrent.classList.contains('cellCurrent-bottom');
-    const right = cellCurrent.classList.contains('cellCurrent-right');
-    const left = cellCurrent.classList.contains('cellCurrent-left');
+    const top = cellCurrent.classList.contains("cellCurrent-top");
+    const bottom = cellCurrent.classList.contains("cellCurrent-bottom");
+    const right = cellCurrent.classList.contains("cellCurrent-right");
+    const left = cellCurrent.classList.contains("cellCurrent-left");
 
     if (top) {
       const currentRow = cellCurrent.parentElement;
       const previousRow = currentRow.previousElementSibling;
       if (previousRow) {
-        const cells = previousRow.querySelectorAll('.cell');
+        const cells = previousRow.querySelectorAll(".cell");
         const cell = cells[cellCurrent.dataset.column];
-        cellCurrent.classList.remove('cellCurrent', 'cellCurrent-top');
-        cell.classList.add('cellCurrent' ,'cellCurrent-top');
+        if (!cell.classList.contains("cellEnd")) {
+          cellCurrent.classList.remove("cellCurrent", "cellCurrent-top");
+          cell.classList.add("cellCurrent", "cellCurrent-top");
+        }
+      } else {
+        alert("Tu es sorti de la grille !");
       }
     } else if (bottom) {
       const currentRow = cellCurrent.parentElement;
       const nextRow = currentRow.nextElementSibling;
       if (nextRow) {
-        const cells = nextRow.querySelectorAll('.cell');
+        const cells = nextRow.querySelectorAll(".cell");
         const cell = cells[cellCurrent.dataset.column];
-        console.log(cellCurrent);
-        cellCurrent.classList.remove('cellCurrent', 'cellCurrent-bottom');
-        cell.classList.add('cellCurrent', 'cellCurrent-bottom');
+        if (!cell.classList.contains("cellEnd")) {
+          cellCurrent.classList.remove("cellCurrent", "cellCurrent-bottom");
+          cell.classList.add("cellCurrent", "cellCurrent-bottom");
+        }
+      } else {
+        alert("Tu es sorti de la grille !");
       }
     } else if (right) {
       const nextCell = cellCurrent.nextElementSibling;
       if (nextCell) {
-        cellCurrent.classList.remove('cellCurrent', 'cellCurrent-right');
-        nextCell.classList.add('cellCurrent', 'cellCurrent-right');
+        if (!nextCell.classList.contains("cellEnd")) {
+          cellCurrent.classList.remove("cellCurrent", "cellCurrent-right");
+          nextCell.classList.add("cellCurrent", "cellCurrent-right");
+        }
+      } else {
+        alert("Tu es sorti de la grille !");
       }
     } else if (left) {
       const previousCell = cellCurrent.previousElementSibling;
       if (previousCell) {
-        cellCurrent.classList.remove('cellCurrent', 'cellCurrent-left');
-        previousCell.classList.add('cellCurrent', 'cellCurrent-left');
+        if (!previousCell.classList.contains("cellEnd")) {
+          cellCurrent.classList.remove("cellCurrent", "cellCurrent-left");
+          previousCell.classList.add("cellCurrent", "cellCurrent-left");
+        }
+      } else {
+        alert("Tu es sorti de la grille !");
       }
     }
   },
 
   turnRight: function () {
     const curseur = document.querySelector(".cellCurrent");
-    if (curseur.classList.contains('cellCurrent-right')) {
-      curseur.classList.remove('cellCurrent-right');
-      curseur.classList.add('cellCurrent-bottom')
-    } else if (curseur.classList.contains('cellCurrent-bottom')) {
-      curseur.classList.remove('cellCurrent-bottom');
-      curseur.classList.add('cellCurrent-left')
-    } else if (curseur.classList.contains('cellCurrent-left')) {
-      curseur.classList.remove('cellCurrent-left');
-      curseur.classList.add('cellCurrent-top')
-    } else if (curseur.classList.contains('cellCurrent-top')) {
-      curseur.classList.remove('cellCurrent-top');
-      curseur.classList.add('cellCurrent-right')
-    };
+    if (curseur.classList.contains("cellCurrent-right")) {
+      curseur.classList.remove("cellCurrent-right");
+      curseur.classList.add("cellCurrent-bottom");
+    } else if (curseur.classList.contains("cellCurrent-bottom")) {
+      curseur.classList.remove("cellCurrent-bottom");
+      curseur.classList.add("cellCurrent-left");
+    } else if (curseur.classList.contains("cellCurrent-left")) {
+      curseur.classList.remove("cellCurrent-left");
+      curseur.classList.add("cellCurrent-top");
+    } else if (curseur.classList.contains("cellCurrent-top")) {
+      curseur.classList.remove("cellCurrent-top");
+      curseur.classList.add("cellCurrent-right");
+    }
   },
 
   turnLeft: function () {
     const curseur = document.querySelector(".cellCurrent");
-    if (curseur.classList.contains('cellCurrent-right')) {
-      curseur.classList.remove('cellCurrent-right');
-      curseur.classList.add('cellCurrent-top')
-    } else if (curseur.classList.contains('cellCurrent-top')) {
-      curseur.classList.remove('cellCurrent-top');
-      curseur.classList.add('cellCurrent-left')
-    } else if (curseur.classList.contains('cellCurrent-left')) {
-      curseur.classList.remove('cellCurrent-left');
-      curseur.classList.add('cellCurrent-bottom')
-    } else if (curseur.classList.contains('cellCurrent-bottom')) {
-      curseur.classList.remove('cellCurrent-bottom');
-      curseur.classList.add('cellCurrent-right')
-    };
+    if (curseur.classList.contains("cellCurrent-right")) {
+      curseur.classList.remove("cellCurrent-right");
+      curseur.classList.add("cellCurrent-top");
+    } else if (curseur.classList.contains("cellCurrent-top")) {
+      curseur.classList.remove("cellCurrent-top");
+      curseur.classList.add("cellCurrent-left");
+    } else if (curseur.classList.contains("cellCurrent-left")) {
+      curseur.classList.remove("cellCurrent-left");
+      curseur.classList.add("cellCurrent-bottom");
+    } else if (curseur.classList.contains("cellCurrent-bottom")) {
+      curseur.classList.remove("cellCurrent-bottom");
+      curseur.classList.add("cellCurrent-right");
+    }
   },
 
   handleLaunchScriptButton: function (event) {
     // TODO
     // on recup la valeur de textarea
     event.preventDefault();
-    const scriptArea = document.querySelector('#userCode');
+    const scriptArea = document.querySelector("#userCode");
     const script = scriptArea.value;
     console.log(script);
-    
 
     // TODO : get all lines as an array
     // on le convertie  en tableau pour passer les commandes pour faire bouger la flèche
-    const codeLines = script.split('\n');
-    console.log(codeLines)
-    console.log(typeof codeLines)
+    const codeLines = script.split("\n");
 
     // Appel, après 2sec, la méthode codeLineLoop() avec la variable et l'index de départ (0)
     window.setTimeout(function () {
@@ -158,12 +159,32 @@ var app = {
     }, 2000);
   },
 
+  handleResetScriptButton: function (event) {
+    event.preventDefault();
+    const cells = document.querySelectorAll('.cell');
+    const board = document.querySelector('#board');
+    board.innerHTML = '';
+    const scriptArea = document.querySelector("#userCode");
+    scriptArea.value = '';
+    cells.forEach(cell => {
+      cell.classList.remove('cellStart', 'cellEnd', 'cellCurrent')
+    })
+    app.drawBoard();
+  },
+
   codeLineLoop: function (codeLines, index) {
     // Getting currentLine
     var currentLine = codeLines[index];
     console.log(currentLine);
+    console.log(index);
 
-
+    if (currentLine == "turnLeft") {
+      app.turnLeft();
+    } else if (currentLine == "turnRight") {
+      app.turnRight();
+    } else if (currentLine == "moveForward") {
+      app.moveForward();
+    }
     // Increment
     index++;
 
@@ -182,7 +203,17 @@ var app = {
 
   checkSuccess: function () {
     // TODO display if the game is won or not
-  }
+    const cellCurrent = document.querySelector(".cellCurrent");
+
+    const row4 = document.querySelector("#row3");
+    const finish = row4.lastChild;
+
+    if (finish.classList.contains("cellCurrent", "cellEnd")) {
+      alert("YOU WIN");
+    } else {
+      alert("YOU LOSE");
+    }
+  },
 };
 
-document.addEventListener('DOMContentLoaded', app.init);
+document.addEventListener("DOMContentLoaded", app.init);
